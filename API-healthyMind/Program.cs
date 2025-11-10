fd2f76277b3000b347dd3768be3fb7019a8d54da
unknown <camilovillalobos252@gmail.com>
2025-11-10 13:41:51 -0500

    agregando servicio para program.cs

--- API-healthyMind/Program.cs
+++ API-healthyMind/Program.cs
@@ -1,6 +1,7 @@
 using API_healthyMind.Data;
 using API_healthyMind.Repositorios.Implementacion;
 using API_healthyMind.Repositorios.Interfaces;
+using API_healthyMind.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,32 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
 Console.WriteLine("Cadena de conexión cargada: " + connStr);
 
-builder.Services.AddScoped<IAprendizFichaRepository, AprendizFichaRepository>();
-builder.Services.AddScoped<IAprendizRepository, AprendizRepository>();
-builder.Services.AddScoped<IAreaRepository, AreaRepository>();
-builder.Services.AddScoped<ICategoriaPreguntasRepository, CategoriaPreguntasRepository>();
-builder.Services.AddScoped<ICategoriaRespuestasRepository, CategoriaRespuestasRepository>();
-builder.Services.AddScoped<ICentroRepository, CentroRepository>();
-builder.Services.AddScoped<ICitasRepository, CitasRepository>();
-builder.Services.AddScoped<ICiudadRepository, CiudadRepository>();
-builder.Services.AddScoped<IDiarioRepository, DiarioRepository>();
-builder.Services.AddScoped<IEmocionesRepository, EmocionesRepository>();
-builder.Services.AddScoped<IEstadoAprendizRepository, EstadoAprendizRepository>();
-builder.Services.AddScoped<IFichaRepository, FichaRepository>();
-builder.Services.AddScoped<INivelFormacionRepository, NivelFormacionRepository>();
-builder.Services.AddScoped<IPaginaDiarioRepository, PaginaDiarioRepository>();
-builder.Services.AddScoped<IPreguntasRepository, PreguntasRepository>();
-builder.Services.AddScoped<IProgramaFormacionRepository, ProgramaFormacionRepository>();
-builder.Services.AddScoped<IPsicologoRepository, PsicologoRepository>();
-builder.Services.AddScoped<IRegionalRepository, RegionalRepository>();
-builder.Services.AddScoped<IRespuestasRepository, RespuestaRepository>();
-builder.Services.AddScoped<ISeguimientoAprendizRepository, SeguimientoAprendizRepository>();
-builder.Services.AddScoped<ITestGeneralRepository, TestGeneralRepository>();
-builder.Services.AddScoped<ITestPreguntasRepository, TestPreguntasRepository>();
-
-
-builder.Services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
-
+builder.Services.AddMyFeatureServices();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ API-healthyMind/Services/MyServiceCollectionExtensions.cs
@@ -0,0 +1,38 @@
+﻿using API_healthyMind.Data;
+using API_healthyMind.Repositorios.Implementacion;
+using API_healthyMind.Repositorios.Interfaces;
+
+namespace API_healthyMind.Services
+{
+    public static class MyServiceCollectionExtensions
+    {
+        public static IServiceCollection AddMyFeatureServices(this IServiceCollection services)
+        {
+            services.AddScoped<IAprendizFichaRepository, AprendizFichaRepository>();
+            services.AddScoped<IAprendizRepository, AprendizRepository>();
+            services.AddScoped<IAreaRepository, AreaRepository>();
+            services.AddScoped<ICategoriaPreguntasRepository, CategoriaPreguntasRepository>();
+            services.AddScoped<ICategoriaRespuestasRepository, CategoriaRespuestasRepository>();
+            services.AddScoped<ICentroRepository, CentroRepository>();
+            services.AddScoped<ICitasRepository, CitasRepository>();
+            services.AddScoped<ICiudadRepository, CiudadRepository>();
+            services.AddScoped<IDiarioRepository, DiarioRepository>();
+            services.AddScoped<IEmocionesRepository, EmocionesRepository>();
+            services.AddScoped<IEstadoAprendizRepository, EstadoAprendizRepository>();
+            services.AddScoped<IFichaRepository, FichaRepository>();
+            services.AddScoped<INivelFormacionRepository, NivelFormacionRepository>();
+            services.AddScoped<IPaginaDiarioRepository, PaginaDiarioRepository>();
+            services.AddScoped<IPreguntasRepository, PreguntasRepository>();
+            services.AddScoped<IProgramaFormacionRepository, ProgramaFormacionRepository>();
+            services.AddScoped<IPsicologoRepository, PsicologoRepository>();
+            services.AddScoped<IRegionalRepository, RegionalRepository>();
+            services.AddScoped<IRespuestasRepository, RespuestaRepository>();
+            services.AddScoped<ISeguimientoAprendizRepository, SeguimientoAprendizRepository>();
+            services.AddScoped<ITestGeneralRepository, TestGeneralRepository>();
+            services.AddScoped<ITestPreguntasRepository, TestPreguntasRepository>();
+
+            services.AddScoped<IUnidadDeTrabajo, UnidadDeTrabajo>();
+            return services;
+        }
+    }
+}
